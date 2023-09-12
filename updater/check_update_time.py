ff54de503ae42c2385497e52f5722fa499041dfc
njaffe <noah0m0jaffe@gmail.com>
2023-09-12 11:40:24 -0400

    Update check_update_time.py

--- updater/check_update_time.py
+++ updater/check_update_time.py
@@ -1,4 +1,16 @@
 import os
 import requests
-with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-  print(requests.get("https://api.weirdgloop.org/exchange").json(), file=fh)
+WORKSPACE = os.getenv("WORKSPACE")
+latest = requests.get("https://api.weirdgloop.org/exchange").json()
+for k in latest:
+  fn = f"{WORKSPACE}/updater/status/{k}"
+  status = None
+  if os.path.isfile(fn):
+    with open(f"{WORKSPACE}/updater/status/{k}",'r') as f:
+      status = f.read().strip()
+  if status in {"UPDATING","READY"}:
+    quit(0)
+  elif latest[k] != status:
+    print(f"{k} is out of date!")
+    with open(f"{WORKSPACE}/updater/status/{k}",'r') as f:
+      f.write("READY")  # Mark as ready to start the update action
